f170c7307c79f19f9d3f956afa51b249edbcc12a
John Romani <john@anthood.com>
2019-01-20 02:58:03 +0100

    destructor

---
+++ ahsplash/src/components/logowall/logowallItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LogoWallItem = (props) => {
+	const { src, alt } = props;
+	return (
+		<li className="hex" >
+
+		
+				<img src={src} alt={alt} />
+		</li>
+	  );
+}
+ 
+export default LogoWallItem;
