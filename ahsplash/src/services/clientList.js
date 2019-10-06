454e006358196a2cbc3dfa43b6be38752b87d89b
John Romani <john@anthood.com>
2019-10-06 16:32:57 +0200

    cleanup and added clients

--- ahsplash/src/services/clientList.js
+++ ahsplash/src/services/clientList.js
@@ -1,31 +1,69 @@
-const brands = [
- { src:"ecchorights@2x.png", alt:"Eccho Rights" },
- { src:"parfym@2x.png", alt:"parfym.se" },
- { src:"otto@2x.png", alt:"Ottos Barnmat" },
- { src:"workaround@2x.png", alt:"Work Around"}, 
- { src:"schoolido@2x.png", alt:"Schoolido" },
- { src:"piraya@2x.png", alt:"Nätverket Piraya" },
- { src:"stefanik@2x.png", alt:"Lindell Stefanik" },
- { src:"gms@2x.png", alt:"Global Models Inc" },
- { src:"natticole@2x.png", alt:"Natticole" },
- { src:"subito@2x.png", alt:"Subito Events" },
- { src:"scania@2x.png", alt:"Scania" },
- { src:"hm@2x.png", alt:"Hennes & Mauritz" },
- { src:"delmonte@2x.png", alt:"Del Monte" },
- { src:"ken@2x.png", alt:"Kammarensemblen" }
+const brands = [{
+    src: "scania@2x.png",
+    alt: "Scania"
+  },
+  {
+    src: "hm@2x.png",
+    alt: "Hennes & Mauritz"
+  },
+  {
+    src: "handelsbanken@2x.png",
+    alt: "Handelsbanken"
+  },
+  {
+    src: "uvisens@2x.png",
+    alt: "uvisens.se"
+  },
+  {
+    src: "ecchorights@2x.png",
+    alt: "Eccho Rights"
+  },
+  {
+    src: "parfym@2x.png",
+    alt: "parfym.se"
+  },
+  {
+    src: "otto@2x.png",
+    alt: "Ottos Barnmat"
+  },
+  {
+    src: "workaround@2x.png",
+    alt: "Work Around"
+  },
+  {
+    src: "schoolido@2x.png",
+    alt: "Schoolido"
+  },
+  {
+    src: "piraya@2x.png",
+    alt: "Nätverket Piraya"
+  },
+  {
+    src: "stefanik@2x.png",
+    alt: "Lindell Stefanik"
+  },
+  {
+    src: "gms@2x.png",
+    alt: "Global Models Inc"
+  },
+  {
+    src: "natticole@2x.png",
+    alt: "Natticole"
+  },
+  {
+    src: "subito@2x.png",
+    alt: "Subito Events"
+  },
+  {
+    src: "delmonte@2x.png",
+    alt: "Del Monte"
+  },
+  {
+    src: "ken@2x.png",
+    alt: "Kammarensemblen"
+  }
 ];
 
 export function getBrands() {
   return brands;
-}
-
-export function getMovie(id) {
-  return brands.find(m => m._id === id);
-}
-
-
-export function deleteMovie(id) {
-  let movieInDb = brands.find(m => m._id === id);
-  brands.splice(brands.indexOf(movieInDb), 1);
-  return movieInDb;
 }
--- ahsplash/src/services/partnerList.js
+++ ahsplash/src/services/partnerList.js
@@ -1,10 +1,21 @@
-const brands = [
-	{ src:"hd@2x.png", alt:"Holy Diver"},
-	{ src:"cc@2x.png", alt:"Champs & Chimps"},
-	{ src:"delta@2x.png", alt:"Delta Web Solutions"},
-	{ src:"moow@2x.png", alt:"Moow"}
+const brands = [{
+		src: "hd@2x.png",
+		alt: "Holy Diver"
+	},
+	{
+		src: "infobahn@2x.png",
+		alt: "Infobahn"
+	},
+	{
+		src: "delta@2x.png",
+		alt: "Delta Web Solutions"
+	},
+	{
+		src: "moow@2x.png",
+		alt: "Moow"
+	}
 ];
 
 export function getBrands() {
-  return brands;
+	return brands;
 }
